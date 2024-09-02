6a9136dbce9b1029867a5ac6ba92b3db1ff75dc5
Daniel Alanis <danielalanis@MacBook-Pro-de-Daniel.local>
2024-09-02 09:58:39 -0300

    se agrega clase 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,59 @@
 import React from "react";
-import Button from "./components/Button";
-import Form from "./components/Form";
 import "./App.css";
-import Container from "./components/Container";
+import Container from "./components/Container/Container";
 
-function App() {
+// componentes basados en clases
+class MyComponent extends React.Component {
+  render() {
+    <div></div>;
+  }
+}
 
-  //Pasar la propiedad label al componente Button
+const App = () => {
+  //object - Key y Value (clave y valor)
+  const obj = {
+    id: 1,
+    title: "Producto 1",
+    description: "Descripción del Producto 1",
+    stock: 10,
+    price: 1000,
+    currencyType: "ARS",
+    myFunction: () => {
+      console.log("test");
+    },
+  };
+
+  const { id, title, description } = obj;
+
+  console.log(id, title, description);
+
+  // === compara valor y tipo de dato
+  // == compara valor --- ejemplo: "10" y 10 : true
+  const number = 10;
+  if (number === 10) {
+    console.log("Es igual a " + number);
+    if (typeof number === "number") {
+      console.log("number");
+    }
+  } else {
+    console.log("No es igual a " + number);
+  }
+
+  console.log(
+    number === 10 ? `Es igual a ${number}` : `No es igual a ${number}`
+  );
+
+  const isRed = false;
+
   return (
     <React.Fragment>
+      {/* <div className={isRed ? "my-class-red" : "my-class-blue"}> */}
       <div>
         <Container buttonLabel="My button form" />
-        {/* <Button label="Login" />
-        <Button label="Contactame" />
-        <Button label="Logout" />
-        <Form buttonLabel="My form Button" /> */}
+        {/* <button onClick={() => (isRed = true)}></button> */}
       </div>
     </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ src/components/Container/Container.styles.js
@@ -0,0 +1,16 @@
+//CSS in JS -- Estilos en Javascript
+export const myStylesPrimary = {
+    backgroundColor: primaryColor ? "red" : "blue",
+    fontSize: "1rem",
+    fontWeight: "bold",
+    color: "blue",
+    display: "flex",
+  };
+  
+export const myStylesSecondary = {
+    backgroundColor: primaryColor ? "blue" : "red",
+    fontSize: "1rem",
+    fontWeight: "light",
+    color: "red",
+    display: "inline",
+  };
