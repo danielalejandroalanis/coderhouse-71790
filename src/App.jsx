c6897eeaeb3bb0e9db26d42da3047f80392d5b2a
Daniel Alanis <danielalanis@MacBook-Pro-de-Daniel.local>
2024-09-04 09:58:03 -0300

    se elimina codigo basura

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,11 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 const App = () => {
-  
+
   return (
     <ChakraProvider>
       <NavBar />
       <ItemListContainer greeting="Bienvenidos a mi tienda" />
-      <div
-        style={{
-          fontSize: "4rem",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-        }}
-      >
-        Valor del Estado: {count}
-        <div style={{ display: "flex" }}>
-          <button onClick={handleAddItem}>+</button>
-          <button onClick={handleRemoveItem}>-</button>
-        </div>
-      </div>
     </ChakraProvider>
   );
 };
